4a59610d7498eec77a49b5c550f4fc55cb50501e
m-ahrukh <72295010+m-ahrukh@users.noreply.github.com>
2024-06-30 22:48:17 +0500

    select in goLang

---
+++ selectChannels/selectChannel.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("select in go lang")
+
+	greenCh := make(chan string)
+	yellowCh := make(chan string)
+	redCh := make(chan string)
+
+	// go green(greenCh)
+	// go yellow(yellowCh)
+	// go red(redCh)
+
+	fmt.Println("Waiting fot mesages.. Blocked here")
+	for {
+		select {
+		case msg := <-greenCh:
+			fmt.Println(msg)
+		case msg := <-yellowCh:
+			fmt.Println(msg)
+		case msg := <-redCh:
+			fmt.Println(msg)
+		}
+	}
+	go green(greenCh)
+	go yellow(yellowCh)
+	go red(redCh)
+}
+
+func green(green chan string) {
+	for {
+		time.Sleep(1 * time.Second)
+		green <- "Green Message"
+	}
+
+}
+
+func yellow(yellow chan string) {
+	for {
+		time.Sleep(3 * time.Second)
+		yellow <- "Yellow Message"
+	}
+}
+
+func red(red chan string) {
+	for {
+		time.Sleep(1 * time.Second)
+		red <- "Green Message"
+	}
+}
