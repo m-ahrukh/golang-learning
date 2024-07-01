e6c3fda9c946866f5f25023ff5e4476db72cf996
m-ahrukh <72295010+m-ahrukh@users.noreply.github.com>
2024-07-01 11:05:22 +0500

    read data from csv file

--- quizGame/quiz.go
+++ quizGame/quiz.go
@@ -1,9 +1,33 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"os"
 )
 
 func main() {
 	fmt.Println("Quiz Game")
+
+	//open specific file in read-only mode
+	file, err := os.Open("problems.csv")
+
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	//close file
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+
+	questions, err := reader.ReadAll()
+
+	if err != nil {
+		fmt.Println("Error in reading questions")
+	}
+
+	for _, question := range questions {
+		fmt.Println(question[0][0], "+", question[0][0], "=", question[1])
+	}
 }
