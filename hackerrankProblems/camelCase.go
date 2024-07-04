c4589041e548cded70841becc600caf987bb7b11
m-ahrukh <72295010+m-ahrukh@users.noreply.github.com>
2024-07-04 11:02:19 +0500

    hackerrank problem1: camelCase

---
+++ hackerrankProblems/camelCase.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"unicode"
+)
+
+func solution() {
+	fmt.Println("-----CamelCase Problem-----")
+
+	fmt.Print("Enter your string: ")
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+
+	count := 1
+	for _, char := range input {
+		if unicode.IsUpper(char) {
+			count++
+		}
+	}
+
+	fmt.Println("Number of words are:", count)
+}
