51136c7b6b1c3be64f54a5532db1d1edd9f61a55
m-ahrukh <72295010+m-ahrukh@users.noreply.github.com>
2024-07-06 18:46:03 +0500

    getting the html of the webpage

--- sitemapBuilder/sitemap.go
+++ sitemapBuilder/sitemap.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+// TODO:
+//     1. get page
+//     2. parse the html of that page and extract links
+//     3. build proper urls
+//     4. filter liks with different domains
+//     5. find pages
+//     6. print xml
 
 func main() {
 	fmt.Println("Sitemap Builder Problem")
+
+	url := flag.String("url", "https://gophercises.com/", "the url that you want to build a sitemap for")
+	flag.Parse()
+
+	fmt.Println("url:", *url)
+
+	response, err := http.Get(*url)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	defer response.Body.Close()
+	io.Copy(os.Stdout, response.Body) //print the html of the webpage
 }
