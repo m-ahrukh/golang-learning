d1027419195340c60b77c8b046a6971cdaf61761
m-ahrukh <72295010+m-ahrukh@users.noreply.github.com>
2024-07-10 12:44:28 +0500

    cli task completed

--- cliTaskManager/cmd/add.go
+++ cliTaskManager/cmd/add.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -10,7 +11,12 @@ var addCmd = &cobra.Command{
 	Use:   "add",
 	Short: "Add task in your list",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Add Command Called")
+		// for i, arg := range args {
+		// 	fmt.Println(i+1, ":", arg)
+		// }
+
+		task := strings.Join(args, " ")
+		fmt.Printf("Task added \"%s\" to your task list\n", task)
 	},
 }
 
--- cliTaskManager/cmd/do.go
+++ cliTaskManager/cmd/do.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/spf13/cobra"
 )
@@ -11,6 +12,16 @@ var doCmd = &cobra.Command{
 	Short: "Marks task as completed in your list",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("do Command Called")
+		var ids []int
+		for _, arg := range args {
+			id, err := strconv.Atoi(arg)
+			if err != nil {
+				fmt.Println("Failed to parse the argument:", arg)
+			} else {
+				ids = append(ids, id)
+			}
+		}
+		fmt.Println(ids)
 	},
 }
 
