cafb4b22141f9e6982a41bfcb74071c0fdbe3431
m-ahrukh <72295010+m-ahrukh@users.noreply.github.com>
2024-07-16 01:55:02 +0500

    recover panic middleware

--- middlewareRecovery/main.go
+++ middlewareRecovery/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"runtime/debug"
 )
 
 func main() {
@@ -11,7 +12,9 @@ func main() {
 	mux.HandleFunc("/panic/", panicDemo)
 	mux.HandleFunc("/panic-after/", panicAfterDemo)
 	mux.HandleFunc("/", hello)
-	log.Fatal(http.ListenAndServe(":3000", mux))
+	// log.Fatal(http.ListenAndServe(":3000", mux))
+
+	log.Fatal(http.ListenAndServe(":3000", recoverPanicMiddleware(mux, true)))
 }
 
 func panicDemo(w http.ResponseWriter, r *http.Request) {
@@ -29,4 +32,22 @@ func funcThatPanics() {
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "<h1>Hello!</h1>")
+}
+
+func recoverPanicMiddleware(app http.Handler, dev bool) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				stack := debug.Stack()
+				log.Println(string(stack))
+				if !dev {
+					http.Error(w, "Something went wrong ", http.StatusInternalServerError)
+					return
+				}
+				fmt.Fprintf(w, "<h1>pnic: %s</h1><pre>%s</pre>", err, string(stack))
+			}
+		}()
+
+		app.ServeHTTP(w, r)
+	}
 }
