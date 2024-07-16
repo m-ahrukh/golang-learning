d9e92be6e1961c7c8d522b30db982cd5d73ae7c5
m-ahrukh <72295010+m-ahrukh@users.noreply.github.com>
2024-07-16 14:02:58 +0500

    added response writer wrapper

--- middlewareRecovery/main.go
+++ middlewareRecovery/main.go
@@ -34,6 +34,34 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "<h1>Hello!</h1>")
 }
 
+type responseWriter struct {
+	http.ResponseWriter
+	writes [][]byte
+	status int
+}
+
+func (rw *responseWriter) Write(b []byte) (int, error) {
+	rw.writes = append(rw.writes, b)
+	return len(b), nil
+}
+
+func (rw *responseWriter) WriteHeader(statusCode int) {
+	rw.status = statusCode
+}
+
+func (rw *responseWriter) flush() error {
+	if rw.status != 0 {
+		rw.ResponseWriter.WriteHeader(rw.status)
+	}
+	for _, write := range rw.writes {
+		_, err := rw.ResponseWriter.Write(write)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 func recoverPanicMiddleware(app http.Handler, dev bool) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
@@ -44,10 +72,13 @@ func recoverPanicMiddleware(app http.Handler, dev bool) http.HandlerFunc {
 					http.Error(w, "Something went wrong ", http.StatusInternalServerError)
 					return
 				}
+				w.WriteHeader(http.StatusInternalServerError)
 				fmt.Fprintf(w, "<h1>pnic: %s</h1><pre>%s</pre>", err, string(stack))
 			}
 		}()
 
-		app.ServeHTTP(w, r)
+		newWriter := &responseWriter{ResponseWriter: w}
+		app.ServeHTTP(newWriter, r)
+		newWriter.flush()
 	}
 }
