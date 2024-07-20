d8b8d162c6627ac6a1859af13f88cb0b0018231d
m-ahrukh <72295010+m-ahrukh@users.noreply.github.com>
2024-07-20 10:45:57 +0500

    add mutiple tests in a single function

--- tdd/hello/hello.go
+++ tdd/hello/hello.go
@@ -9,10 +9,38 @@ func main() {
 }
 */
 
+/*
+//function without any parameter
 func Hello() string {
 	return "Hello, World"
 }
+*/
+
+/*
+//function with parameter name
+func Hello(name string) string {
+	return "Hello, " + name
+}
+*/
+
+/*
+//replace string with Constant
+const englishHelloPrefix = "Hello, "
+
+func Hello(name string) string {
+	return englishHelloPrefix + name
+}
+*/
+
+const englishHelloPrefix = "Hello, "
+
+func Hello(name string) string {
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
+}
 
 func main() {
-	fmt.Println(Hello())
+	fmt.Println(Hello("Mahrukh"))
 }
--- tdd/hello/hello_test.go
+++ tdd/hello/hello_test.go
@@ -6,9 +6,43 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+/*
+//function without any parameter
 func TestHello(t *testing.T) {
 	got := Hello()
 	want := "Hello, World"
 
 	assert.Equal(t, want, got)
+}
+*/
+
+/*
+// Test with parameter
+func TestHello(t *testing.T) {
+	got := Hello("Mahrukh")
+	want := "Hello, Mahrukh"
+
+	assert.Equal(t, want, got)
+}
+*/
+
+func TestHello(t *testing.T) {
+	t.Run("Saying hello to people", func(t *testing.T) {
+		got := Hello("Mahrukh")
+		want := "Hello, Mahrukh"
+
+		assertCorrectMessage(t, want, got)
+	})
+
+	t.Run("empty string defaults to 'World'", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, want, got)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	assert.Equal(t, got, want)
 }
