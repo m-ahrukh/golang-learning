97d0142054ac198061655d4da6dcbfe735692348
m-ahrukh <72295010+m-ahrukh@users.noreply.github.com>
2024-07-20 14:54:37 +0500

    testing with structs

--- tdd/structsMethodsAndInterfaces/interfaces.go
+++ tdd/structsMethodsAndInterfaces/interfaces.go
@@ -1,9 +1,34 @@
 package structsmethodsandinterfaces
 
+import "math"
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+/*
 func Perimeter(height float64, width float64) float64 {
 	return 2*height + 2*width
 }
 
 func Area(height float64, width float64) float64 {
 	return height * width
+}
+*/
+
+func (r Rectangle) Perimeter() float64 {
+	return 2*r.Height + 2*r.Width
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
--- tdd/structsMethodsAndInterfaces/interfaces_test.go
+++ tdd/structsMethodsAndInterfaces/interfaces_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+/*
 func TestPerimeter(t *testing.T) {
 	got := Perimeter(10.0, 10.0)
 	want := 40.0
@@ -18,4 +19,39 @@ func TestArea(t *testing.T) {
 	want := 72.0
 
 	assert.Equal(t, got, want)
+}
+*/
+
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := rectangle.Perimeter()
+	want := 40.0
+
+	assert.Equal(t, want, got)
+}
+
+func TestArea(t *testing.T) {
+	rectangle := Rectangle{12.0, 6.0}
+	got := rectangle.Area()
+	want := 72.0
+
+	assert.Equal(t, want, got)
+}
+
+func TestAreas(t *testing.T) {
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		got := rectangle.Area()
+		want := 72.0
+
+		assert.Equal(t, want, got)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		assert.Equal(t, want, got)
+	})
 }
