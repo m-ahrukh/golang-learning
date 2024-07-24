fe38cb14a647aeb91f63371ee64788e76616a355
m-ahrukh <72295010+m-ahrukh@users.noreply.github.com>
2024-07-24 11:40:40 +0500

    context testing

--- tdd/contexts/context.go
+++ tdd/contexts/context.go
@@ -19,7 +19,12 @@ func Server(store Store) http.HandlerFunc {
 
 	return func(w http.ResponseWriter, r *http.Request) {
 
-		data, _ := store.Fetch(r.Context())
+		data, err := store.Fetch(r.Context())
+
+		if err != nil {
+			return
+		}
+
 		fmt.Fprint(w, data)
 
 		// ctx := r.Context()
--- tdd/contexts/context_test.go
+++ tdd/contexts/context_test.go
@@ -2,6 +2,7 @@ package contexts
 
 import (
 	"context"
+	"errors"
 	"log"
 	"net/http"
 	"net/http/httptest"
@@ -15,6 +16,24 @@ type SpyStore struct {
 	response string
 	// cancelled bool
 	t *testing.T
+}
+
+type SpyResponseWriter struct {
+	written bool
+}
+
+func (s *SpyResponseWriter) Header() http.Header {
+	s.written = true
+	return nil
+}
+
+func (s *SpyResponseWriter) Write([]byte) (int, error) {
+	s.written = true
+	return 0, errors.New("not implemented")
+}
+
+func (s *SpyResponseWriter) WriteHeader(statusCode int) {
+	s.written = true
 }
 
 // func (s *SpyStore) Fetch() string {
@@ -133,5 +152,21 @@ func TestServer(t *testing.T) {
 
 		got := response.Body.String()
 		assert.Equal(t, data, got)
+	})
+
+	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
+		data := "hello, world"
+		store := &SpyStore{response: data, t: t}
+		svr := Server(store)
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+		cancellingCtx, cancel := context.WithCancel(request.Context())
+		time.AfterFunc(5*time.Millisecond, cancel)
+		request = request.WithContext(cancellingCtx)
+		response := &SpyResponseWriter{}
+		svr.ServeHTTP(response, request)
+		if response.written {
+			t.Error("a response should not have been written")
+		}
 	})
 }
