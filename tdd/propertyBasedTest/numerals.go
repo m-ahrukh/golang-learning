0ef0b9191faa6ac22c6b36345835ea7451de6576
m-ahrukh <72295010+m-ahrukh@users.noreply.github.com>
2024-07-24 17:17:34 +0500

    conversion on number till 50

--- tdd/propertyBasedTest/numerals.go
+++ tdd/propertyBasedTest/numerals.go
@@ -2,6 +2,21 @@ package propertybasedtest
 
 import "strings"
 
+type RomanNumeral struct {
+	Value  int
+	Symbol string
+}
+
+var RomanNumerals = []RomanNumeral{
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
 func ConvertToRoman(arabic int) string {
 	// if arabic == 3 {
 	// 	return "III"
@@ -27,20 +42,27 @@ func ConvertToRoman(arabic int) string {
 	// 	result.WriteString("I")
 	// }
 
-	for arabic > 0 {
-		switch {
-		case arabic > 8:
-			result.WriteString("IX")
-			arabic -= 9
-		case arabic > 4:
-			result.WriteString("V")
-			arabic -= 5
-		case arabic > 3:
-			result.WriteString("IV")
-			arabic -= 4
-		default:
-			result.WriteString("I")
-			arabic--
+	// for arabic > 0 {
+	// 	switch {
+	// 	case arabic > 8:
+	// 		result.WriteString("IX")
+	// 		arabic -= 9
+	// 	case arabic > 4:
+	// 		result.WriteString("V")
+	// 		arabic -= 5
+	// 	case arabic > 3:
+	// 		result.WriteString("IV")
+	// 		arabic -= 4
+	// 	default:
+	// 		result.WriteString("I")
+	// 		arabic--
+	// 	}
+	// }
+
+	for _, numeral := range RomanNumerals {
+		for arabic >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
 		}
 	}
 
