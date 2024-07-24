01d9939f904958f75f7ef544821130af67b9948c
m-ahrukh <72295010+m-ahrukh@users.noreply.github.com>
2024-07-24 16:57:33 +0500

    conversion of number from 1 to 9 in roman

--- tdd/propertyBasedTest/numerals.go
+++ tdd/propertyBasedTest/numerals.go
@@ -1,11 +1,48 @@
 package propertybasedtest
 
+import "strings"
+
 func ConvertToRoman(arabic int) string {
-	if arabic == 3 {
-		return "III"
+	// if arabic == 3 {
+	// 	return "III"
+	// }
+	// if arabic == 2 {
+	// 	return "II"
+	// }
+	// return "I"
+
+	var result strings.Builder
+
+	// for i := arabic; i > 0; i-- {
+	// 	if i == 5 {
+	// 		result.WriteString("V")
+	// 		break
+	// 	}
+
+	// 	if i == 4 {
+	// 		result.WriteString("IV")
+	// 		break
+	// 	}
+
+	// 	result.WriteString("I")
+	// }
+
+	for arabic > 0 {
+		switch {
+		case arabic > 8:
+			result.WriteString("IX")
+			arabic -= 9
+		case arabic > 4:
+			result.WriteString("V")
+			arabic -= 5
+		case arabic > 3:
+			result.WriteString("IV")
+			arabic -= 4
+		default:
+			result.WriteString("I")
+			arabic--
+		}
 	}
-	if arabic == 2 {
-		return "II"
-	}
-	return "I"
+
+	return result.String()
 }
--- tdd/propertyBasedTest/numerals_test.go
+++ tdd/propertyBasedTest/numerals_test.go
@@ -33,6 +33,10 @@ func TestRomanNumerals(t *testing.T) {
 		{"1 gets converted to I", 1, "I"},
 		{"2 gets converted to II", 2, "II"},
 		{"3 gets converted to III", 3, "III"},
+		{"4 gets converted to IV", 4, "IV"},
+		{"5 gets converted to V", 5, "V"},
+		{"6 gets converted to VI", 6, "VI"},
+		{"9 gets converted to IX", 9, "IX"},
 	}
 
 	for _, test := range cases {
