208486835005ce463d54ec90c8ffd824f5387d11
m-ahrukh <72295010+m-ahrukh@users.noreply.github.com>
2024-07-25 12:55:30 +0500

    testing Seconds hand of clock

---
+++ tdd/maths/clockface/clockface_test.go
@@ -0,0 +1,35 @@
+package clockface
+
+import (
+	"math"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSecondHandAtMidnight(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+
+	want := Point{X: 150, Y: 150 - 90}
+	got := SecondHand(tm)
+
+	assert.Equal(t, want, got)
+}
+
+// func TestSecondHandAt30Seconds(t *testing.T) {
+// 	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
+
+// 	want := Point{X: 150, Y: 150 + 90}
+// 	got := SecondHand(tm)
+
+// 	assert.Equal(t, want, got)
+// }
+
+func TestSecondsInRadians(t *testing.T) {
+	thirtySeconds := time.Date(312, time.October, 28, 0, 0, 30, 0, time.UTC)
+	want := math.Pi
+	got := secondsInRadians(thirtySeconds)
+
+	assert.Equal(t, want, got)
+}
