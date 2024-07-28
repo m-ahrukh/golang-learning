73670eecfb807f7cb2cb4015f57def182d9b5383
m-ahrukh <72295010+m-ahrukh@users.noreply.github.com>
2024-07-28 13:44:07 +0500

    return players score form PlayerServer

---
+++ tdd/gamePlayersWon/server_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGETPlayers(t *testing.T) {
+	t.Run("returns Pepper's score", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/players/Pepper", nil)
+		response := httptest.NewRecorder()
+
+		PlayerServer(response, request)
+
+		got := response.Body.String()
+		want := "20"
+
+		assert.Equal(t, want, got)
+	})
+
+	t.Run("returns Floyd's score", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/players/Floyd", nil)
+		response := httptest.NewRecorder()
+
+		PlayerServer(response, request)
+
+		got := response.Body.String()
+		want := "10"
+
+		assert.Equal(t, want, got)
+	})
+}
