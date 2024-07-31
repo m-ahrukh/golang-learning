5c23ef3ae4cc31535f6689bdd78c11980565bca0
m-ahrukh <72295010+m-ahrukh@users.noreply.github.com>
2024-07-31 19:25:01 +0500

    added emty file

--- tdd/gamePlayersWon/file_system_store.go
+++ tdd/gamePlayersWon/file_system_store.go
@@ -13,7 +13,17 @@ type FileSystemPlayerStore struct {
 }
 
 func NewFileSystemPlayerStore(database *os.File) (*FileSystemPlayerStore, error) {
+
 	database.Seek(0, io.SeekStart)
+	info, err := database.Stat()
+	if err != nil {
+		return nil, fmt.Errorf("problem getting file info from file %s, %v", database.Name(), err)
+	}
+	if info.Size() == 0 {
+		database.Write([]byte("[]"))
+		database.Seek(0, io.SeekStart)
+	}
+
 	league, err := NewLeague(database)
 	if err != nil {
 		return nil, fmt.Errorf("problem loading player store from file %s, %v", database.Name(), err)
