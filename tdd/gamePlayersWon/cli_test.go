f7bb50cee7a9ed3d0bef01763adbea440aa9daf5
m-ahrukh <72295010+m-ahrukh@users.noreply.github.com>
2024-08-01 12:14:40 +0500

    records player wins from user input

--- tdd/gamePlayersWon/cli_test.go
+++ tdd/gamePlayersWon/cli_test.go
@@ -6,21 +6,24 @@ import (
 )
 
 func TestCLI(t *testing.T) {
-	in := strings.NewReader("Chris wins\n")
-	playerStore := &StubPlayerStore{}
+	t.Run("record chris win from user input", func(t *testing.T) {
+		in := strings.NewReader("Chris wins\n")
+		playerStore := &StubPlayerStore{}
 
-	cli := &CLI{playerStore, in}
-	cli.PlayPoker()
+		cli := &CLI{playerStore: playerStore, in: in}
+		cli.PlayPoker()
 
-	if len(playerStore.winCalls) != 1 {
-		t.Fatal("expected a win call but didn't get any")
-	}
+		assertPlayerWin(t, playerStore, "Chris")
+	})
 
-	got := playerStore.winCalls[0]
-	want := "Chris"
+	t.Run("record cleo win from user input", func(t *testing.T) {
+		in := strings.NewReader("Cleo wins\n")
+		playerStore := &StubPlayerStore{}
 
-	if got != want {
-		t.Errorf("didn't record correct winner, got %q, want %q", got, want)
-	}
+		cli := &CLI{playerStore: playerStore, in: in}
+		cli.PlayPoker()
+
+		assertPlayerWin(t, playerStore, "Cleo")
+	})
 
 }
--- tdd/gamePlayersWon/server_test.go
+++ tdd/gamePlayersWon/server_test.go
@@ -157,6 +157,18 @@ func TestLeague(t *testing.T) {
 	})
 }
 
+func assertPlayerWin(t testing.TB, store *StubPlayerStore, winner string) {
+	t.Helper()
+
+	if len(store.winCalls) != 1 {
+		t.Fatalf("got %d calls to RecordWin want %d", len(store.winCalls), 1)
+	}
+
+	if store.winCalls[0] != winner {
+		t.Errorf("did not store correct winner got %q want %q", store.winCalls[0], winner)
+	}
+}
+
 func assertContentType(t testing.TB, response *httptest.ResponseRecorder, want string) {
 	t.Helper()
 	if response.Result().Header.Get("content-type") != want {
