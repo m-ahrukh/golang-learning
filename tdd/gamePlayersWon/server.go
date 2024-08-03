553c1dba95b32569d40d1d2837086f41293d772f
m-ahrukh <72295010+m-ahrukh@users.noreply.github.com>
2024-08-03 16:36:14 +0500

    added game route

--- tdd/gamePlayersWon/server.go
+++ tdd/gamePlayersWon/server.go
@@ -31,12 +31,17 @@ func NewPlayerServer(store PlayerStore) *PlayerServer {
 	p.store = store
 
 	router := http.NewServeMux()
+	router.Handle("/game", http.HandlerFunc(p.game))
 	router.Handle("/league", http.HandlerFunc(p.leagueHandler))
 	router.Handle("/players/", http.HandlerFunc(p.playerHandler))
 
 	p.Handler = router
 
 	return p
+}
+
+func (p *PlayerServer) game(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
 }
 
 func (p *PlayerServer) leagueHandler(w http.ResponseWriter, r *http.Request) {
--- tdd/gamePlayersWon/server_test.go
+++ tdd/gamePlayersWon/server_test.go
@@ -128,6 +128,19 @@ func TestLeague(t *testing.T) {
 	})
 }
 
+func TestGame(t *testing.T) {
+	t.Run("GET /game returns 200", func(t *testing.T) {
+		server := NewPlayerServer(&StubPlayerStore{})
+
+		request, _ := http.NewRequest(http.MethodGet, "/game", nil)
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, http.StatusOK)
+	})
+}
+
 func getLeagueFromResponse(t *testing.T, body io.Reader) (league []Player) {
 	t.Helper()
 	err := json.NewDecoder(body).Decode(&league)
