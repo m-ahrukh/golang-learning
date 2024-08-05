a0b1aabe132945f121647c0e98d7b79b1670bf23
m-ahrukh <72295010+m-ahrukh@users.noreply.github.com>
2024-08-05 11:33:35 +0500

    added Lsten method

--- smartystreets/processor/nothing_test.go
+++ smartystreets/processor/nothing_test.go
@@ -2,6 +2,36 @@ package processor
 
 import "testing"
 
-func Test(t *testing.T) {
-	t.Log("Hi")
+type Verifier struct {
+	in  chan interface{}
+	out chan interface{}
+}
+
+func NewVerifier(in, out chan interface{}) *Verifier {
+	return &Verifier{
+		in:  in,
+		out: out,
+	}
+}
+
+func (verifier *Verifier) Listen() {
+	verifier.out <- 0
+
+}
+
+func TestVeriferRecievesInput(t *testing.T) {
+	in := make(chan interface{}, 10)
+	out := make(chan interface{}, 10)
+	verifier := NewVerifier(in, out)
+
+	in <- 1
+
+	close(in)
+
+	verifier.Listen()
+
+	recieved := <-out
+	if recieved != 1 {
+		t.Errorf("\nGot %v\nwant: 1", recieved)
+	}
 }
