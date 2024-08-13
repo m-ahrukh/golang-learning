24e9845c6555ccbfe776b50e33629ba214f0e67e
m-ahrukh <72295010+m-ahrukh@users.noreply.github.com>
2024-08-13 18:14:37 +0500

    testing all envelopes writted

--- smartystreets/processor/writer_handler.go
+++ smartystreets/processor/writer_handler.go
@@ -22,20 +22,31 @@ func NewWriterHandler(input chan *Envelope, output io.WriteCloser) *WriterHandle
 func (wh *WriterHandler) Handle() {
 	wh.writer.Write([]string{"Status", "DeliveryLine1", "City", "State", "ZIPCode"})
 
-	envelope := <-wh.input
-
-	if envelope != nil {
+	for envelope := range wh.input {
 		output := envelope.Output
-		wh.writer.Write([]string{
-			output.Status,
-			output.DeliveryLine1,
-			output.City,
-			output.State,
-			output.ZIPCode,
-			output.LastLine,
-		})
+		wh.writeAddressOutput(output)
 	}
+
+	// envelope := <-wh.input
+	// if envelope != nil {
+	// 	output := envelope.Output
+	// 	wh.writeAddressOutput(output)
+	// }
 
 	wh.writer.Flush()
 	wh.closer.Close()
+}
+
+func (wh *WriterHandler) writeAddressOutput(output AddressOutput) {
+	wh.writeValues(
+		output.Status,
+		output.DeliveryLine1,
+		output.City,
+		output.LastLine,
+		output.State,
+		output.ZIPCode,
+	)
+}
+func (wh WriterHandler) writeValues(values ...string) {
+	wh.writer.Write(values)
 }
--- smartystreets/processor/writer_handler_test.go
+++ smartystreets/processor/writer_handler_test.go
@@ -49,9 +49,9 @@ func (whf *WriterHandlerFixture) TestEnvelopeWritten() {
 			Status:        "A",
 			DeliveryLine1: "B",
 			City:          "C",
-			State:         "D",
-			ZIPCode:       "E",
-			LastLine:      "F",
+			LastLine:      "D",
+			State:         "E",
+			ZIPCode:       "F",
 		},
 	}
 	close(whf.input)
@@ -61,6 +61,38 @@ func (whf *WriterHandlerFixture) TestEnvelopeWritten() {
 	lines := strings.Split(outputFile, "\n")
 	if whf.So(lines, should.HaveLength, 2) {
 		whf.So(lines[1], should.Equal, "A,B,C,D,E,F")
+	}
+}
+
+func (whf *WriterHandlerFixture) TestAllEnvelopeWritten() {
+	whf.input <- &Envelope{
+		Output: AddressOutput{
+			Status:        "A1",
+			DeliveryLine1: "B1",
+			City:          "C1",
+			LastLine:      "D1",
+			State:         "E1",
+			ZIPCode:       "F1",
+		},
+	}
+	whf.input <- &Envelope{
+		Output: AddressOutput{
+			Status:        "A2",
+			DeliveryLine1: "B2",
+			City:          "C2",
+			LastLine:      "D2",
+			State:         "E2",
+			ZIPCode:       "F2",
+		},
+	}
+	close(whf.input)
+
+	whf.handler.Handle()
+	outputFile := strings.TrimSpace(whf.buffer.String())
+	lines := strings.Split(outputFile, "\n")
+	if whf.So(lines, should.HaveLength, 3) {
+		whf.So(lines[1], should.Equal, "A1,B1,C1,D1,E1,F1")
+		whf.So(lines[2], should.Equal, "A2,B2,C2,D2,E2,F2")
 	}
 }
 
