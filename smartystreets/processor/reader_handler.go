99df6c92fac5ee7f3c44b60cee2a3ce1d3a345df
m-ahrukh <72295010+m-ahrukh@users.noreply.github.com>
2024-08-14 19:26:36 +0500

    all csv records written to output

--- smartystreets/processor/reader_handler.go
+++ smartystreets/processor/reader_handler.go
@@ -22,17 +22,26 @@ func NewReaderHandler(reader io.ReadCloser, output chan *Envelope) *ReaderHandle
 func (rh *ReaderHandler) Handle() {
 	rh.skipHeader()
 
-	record, _ := rh.reader.Read()
-	envelope := &Envelope{
-		Input: AddressInput{
-			Street1: record[0],
-			City:    record[1],
-			State:   record[2],
-			ZIPCode: record[3],
-		},
+	for {
+		record, err := rh.reader.Read()
+		if err == io.EOF {
+			break
+		} else {
+			//TODO: warn user of malformed file???
+		}
+		rh.output <- &Envelope{
+			Input: createinput(record),
+		}
 	}
+}
 
-	rh.output <- envelope
+func createinput(record []string) AddressInput {
+	return AddressInput{
+		Street1: record[0],
+		City:    record[1],
+		State:   record[2],
+		ZIPCode: record[3],
+	}
 }
 
 func (rh *ReaderHandler) skipHeader() {
--- smartystreets/processor/reader_handler_test.go
+++ smartystreets/processor/reader_handler_test.go
@@ -13,23 +13,54 @@ func TestReaderHandlerFixture(t *testing.T) {
 
 type ReaderHandlerFixture struct {
 	*gunit.Fixture
+
+	buffer *ReadWriteSpyBuffer
+	output chan *Envelope
+	reader *ReaderHandler
 }
 
 func (rhf *ReaderHandlerFixture) Setup() {
+	rhf.buffer = NewReadWriteSpyBuffer("")
+	rhf.output = make(chan *Envelope, 10)
+	rhf.reader = NewReaderHandler(rhf.buffer, rhf.output)
 
+	const header = "Street1,City,State,ZIPCode"
+	rhf.writeLine(header)
 }
 
 func (rhf *ReaderHandlerFixture) TestCSVRecordSentInEnvelope() {
-	buffer := NewReadWriteSpyBuffer("Street1,City,State,ZIPCode\n" + "A,B,C,D\n")
-	output := make(chan *Envelope, 10)
-	reader := NewReaderHandler(buffer, output)
+	rhf.writeLine("A,B,C,D")
 
-	reader.Handle()
+	rhf.reader.Handle()
 
-	rhf.So(<-output, should.Resemble, &Envelope{Input: AddressInput{
+	rhf.So(<-rhf.output, should.Resemble, &Envelope{Input: AddressInput{
 		Street1: "A",
 		City:    "B",
 		State:   "C",
 		ZIPCode: "D",
 	}})
+}
+
+func (rhf *ReaderHandlerFixture) TestAllCSVRecordsWrittenToOutput() {
+	rhf.writeLine("A1,B1,C1,D1")
+	rhf.writeLine("A2,B2,C2,D2")
+
+	rhf.reader.Handle()
+
+	rhf.So(<-rhf.output, should.Resemble, &Envelope{Input: AddressInput{
+		Street1: "A1",
+		City:    "B1",
+		State:   "C1",
+		ZIPCode: "D1",
+	}})
+	rhf.So(<-rhf.output, should.Resemble, &Envelope{Input: AddressInput{
+		Street1: "A2",
+		City:    "B2",
+		State:   "C2",
+		ZIPCode: "D2",
+	}})
+}
+
+func (rhf *ReaderHandlerFixture) writeLine(line string) {
+	rhf.buffer.WriteString(line + "\n")
 }
